9fb4ca1d4ec8e45fe5db2737e2b2edf8060dc74c
Владимир <wRadchuk@gmail.com>
2024-04-10 13:28:38 +0300

    [*] fix камера не скачет если карта уровня меньше размера экрана, перемещение через джойстик блокируется

--- game_engine/src/main/java/com/wradchuk/game_engine/objects/Camera.kt
+++ game_engine/src/main/java/com/wradchuk/game_engine/objects/Camera.kt
@@ -19,10 +19,10 @@ import com.wradchuk.game_engine.utils.getDisplayWidthInPixels
  * @property joystick Джойстик для управления камерой.
  */
 class Camera constructor(
-    override var position: Vector2<Double>,
     private val context: Context,
     private val mapSize: Rect,
     private val joystick: Joystick,
+    override var position: Vector2<Double> = Vector2((context.getDisplayWidthInPixels() / 2).toDouble(),(context.getDisplayHeightInPixels() / 2).toDouble()),
 ): GameObject(position) {
 
     /**
@@ -61,19 +61,34 @@ class Camera constructor(
         val nextPositionX = position.x + velocity.x
         val nextPositionY = position.y + velocity.y
 
-        // Проверяем, чтобы камера не выходила за пределы игровой карты
-        if(nextPositionX > (mapSize.left+halfScreenWidth) && nextPositionX < (mapSize.right-halfScreenWidth))
-            position.x = nextPositionX
-        else {
-            if(nextPositionX < (mapSize.left+halfScreenWidth)) position.x = (mapSize.left+halfScreenWidth).toDouble()
-            if(nextPositionX > (mapSize.right-halfScreenWidth)) position.x = (mapSize.right-halfScreenWidth).toDouble()
+
+        if(mapSize.right < context.getDisplayWidthInPixels()) {
+            position.x = halfScreenWidth.toDouble()
         }
-        if(nextPositionY > (mapSize.top+halfScreenHeight) && nextPositionY < (mapSize.bottom-halfScreenHeight))
-            position.y = nextPositionY
         else {
-            if(nextPositionY < (mapSize.top+halfScreenHeight)) position.y = (mapSize.top+halfScreenHeight).toDouble()
-            if(nextPositionY > (mapSize.bottom-halfScreenHeight)) position.y = (mapSize.bottom-halfScreenHeight).toDouble()
+            // Проверяем, чтобы камера не выходила за пределы игровой карты
+            if(nextPositionX > (mapSize.left+halfScreenWidth) && nextPositionX < (mapSize.right-halfScreenWidth))
+                position.x = nextPositionX
+            else {
+                if(nextPositionX < (mapSize.left+halfScreenWidth)) position.x = (mapSize.left+halfScreenWidth).toDouble()
+                if(nextPositionX > (mapSize.right-halfScreenWidth)) position.x = (mapSize.right-halfScreenWidth).toDouble()
+            }
         }
+
+        if(mapSize.bottom < context.getDisplayHeightInPixels()) {
+            position.y = halfScreenHeight.toDouble()
+        }
+        else {
+            if(nextPositionY > (mapSize.top+halfScreenHeight) && nextPositionY < (mapSize.bottom-halfScreenHeight))
+                position.y = nextPositionY
+            else {
+                if(nextPositionY < (mapSize.top+halfScreenHeight)) position.y = (mapSize.top+halfScreenHeight).toDouble()
+                if(nextPositionY > (mapSize.bottom-halfScreenHeight)) position.y = (mapSize.bottom-halfScreenHeight).toDouble()
+            }
+        }
+
+
+
     }
 
     companion object {
